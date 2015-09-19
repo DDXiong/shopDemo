8b38334087f6b112d8e72114c33032e7bd4b82ee
fd <670289978@qq.com>
2015-09-19 15:59:55 +0800

    shopDemo

---
+++ Demo/shop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shop
+//
+//  Created by tarena500 on 15/9/19.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
